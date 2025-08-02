6ff5439502bb75768912118663c9126d0d50e50c
OleksandrBazylevych <oleksandrbazylevych23@gmail.com>
2025-08-03 01:12:35 +0300

    add content to home page

--- src/pages/Home/ui/HomePage.tsx
+++ src/pages/Home/ui/HomePage.tsx
@@ -6,7 +6,7 @@ const HomePage = () => {
   return (
     <div className={styles.pageWrapper}>
       <Header />
-
+      <main className={styles.content}>hello</main>
       <Footer />
     </div>
   );
